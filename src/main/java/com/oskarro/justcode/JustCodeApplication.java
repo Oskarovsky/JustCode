f00a1d417d709c0950ae08c8996d6ccefc4dbb8b
oskarro <oskar.slyk@gmail.com>
2019-04-27 15:19:44 +0200

    Initial commit

---
+++ src/main/java/com/oskarro/justcode/JustCodeApplication.java
@@ -0,0 +1,13 @@
+package com.oskarro.justcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oskarro/justcode/JustCodeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oskarro.justcode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JustCodeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
