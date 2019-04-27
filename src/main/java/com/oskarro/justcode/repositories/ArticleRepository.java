50403df4f92226dd672371d61e59246369d2dbbe
oskarro <oskar.slyk@gmail.com>
2019-04-27 18:32:36 +0200

    Fix

--- src/main/java/com/oskarro/justcode/repositories/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.oskarro.justcode.repositories;
-
-import com.oskarro.justcode.domains.Article;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ArticleRepository extends CrudRepository<Article, Long> {
-}
--- src/main/java/com/oskarro/justcode/repositories/MessageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.oskarro.justcode.repositories;
-
-import com.oskarro.justcode.domains.Message;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MessageRepository extends CrudRepository<Message, Long> {
-
-}
