bddcb71fde99d8e4115ff1da0d7bfaad1f671e8f
oskarro <oskar.slyk@gmail.com>
2019-04-27 17:41:46 +0200

    Edited WebConfig class

--- src/main/java/com/oskarro/justcode/config/WebConfig.java
+++ src/main/java/com/oskarro/justcode/config/WebConfig.java
@@ -1,4 +1,42 @@
 package com.oskarro.justcode.config;
 
-public class WebConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.validation.Validator;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private MessageSource messageSource;
+
+    @Autowired
+    public WebConfig(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/admin/home").setViewName("adminhome");
+    }
+
+    @Override
+    public Validator getValidator() {
+        LocalValidatorFactoryBean factory = new LocalValidatorFactoryBean();
+        factory.setValidationMessageSource(messageSource);
+        return factory;
+    }
+
+    @Bean
+    public SpringSecurityDialect securityDialect() {
+        return new SpringSecurityDialect();
+    }
+
 }
