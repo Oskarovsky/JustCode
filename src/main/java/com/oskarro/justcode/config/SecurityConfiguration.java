a5ae467747703c8e819f831a4c4cdf672a8cdea5
oskarro <oskar.slyk@gmail.com>
2019-04-30 17:25:20 +0200

    Web Config modified

--- src/main/java/com/oskarro/justcode/config/SecurityConfiguration.java
+++ src/main/java/com/oskarro/justcode/config/SecurityConfiguration.java
@@ -1,24 +1,40 @@
 package com.oskarro.justcode.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .authorizeRequests()
-                .antMatchers("/**")
-                .permitAll()
+                .antMatchers("/", "/articles", "/article/show", "/console/**", "/h2/**").permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .authorizeRequests()
-                .antMatchers("/h2/**")
-                .permitAll();
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout().permitAll();
 
         httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
+    }
+
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("ADMIN")
+                .and()
+                .withUser("user").password("user").roles("USER");
     }
 }
