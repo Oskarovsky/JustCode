bfaf6a362715b96b515aedf0b9e4cbb30f188d66
oskarro <oskar.slyk@gmail.com>
2019-04-30 11:01:11 +0200

    Replace HashSet by String

--- src/main/java/com/oskarro/justcode/bootstrap/ArticleLoader.java
+++ src/main/java/com/oskarro/justcode/bootstrap/ArticleLoader.java
@@ -2,25 +2,20 @@ package com.oskarro.justcode.bootstrap;
 
 import com.oskarro.justcode.domains.Article;
 import com.oskarro.justcode.repositories.ArticleRepository;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.Data;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
-@Getter
-@Setter
+@Data
 @Component
 public class ArticleLoader implements ApplicationListener<ContextRefreshedEvent> {
 
-    private ArticleRepository articleRepository;
+    private final ArticleRepository articleRepository;
 
-    @Autowired
-    public void setArticleRepository(ArticleRepository articleRepository) {
+    public ArticleLoader(ArticleRepository articleRepository) {
         this.articleRepository = articleRepository;
     }
-
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
@@ -30,12 +25,24 @@ public class ArticleLoader implements ApplicationListener<ContextRefreshedEvent>
         firstPost.setTitle("My first blog post!");
         firstPost.setContent("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor " +
                 "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation");
+        firstPost.setCategory("Private");
         articleRepository.save(firstPost);
 
         Article secondPost = new Article();
         secondPost.setTitle("Java is amazing");
         secondPost.setContent("No, not really. It has us writing SQL statements. What if you’re just doing a " +
                 "prototype? If you");
+        secondPost.setCategory("Java");
         articleRepository.save(secondPost);
+
+
+
+
+
+
+
+
+
     }
+
 }
--- src/main/java/com/oskarro/justcode/domains/Article.java
+++ src/main/java/com/oskarro/justcode/domains/Article.java
@@ -1,24 +1,26 @@
 package com.oskarro.justcode.domains;
 
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Getter
-@Setter
+@Data
 @Entity
 public class Article {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
     private String content;
+    private String category;
+
+
+
 
 }
